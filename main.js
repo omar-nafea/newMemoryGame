252c841cf32466e294fcf8360f26eb5092948743
omar-nafea <omarahmednafea248@gmail.com>
2022-08-03 16:14:23 +0200

    updated

--- main.js
+++ main.js
@@ -1,160 +1,56 @@
-// Select The Start Game Button
 document.querySelector(".control-buttons").onclick = function() {
     document.querySelector(".control-buttons").remove();
-
 };
-
-// Effect Duration
-let duration = 500;
-
-// Select Blocks Container
+let duration = 800;
 let blocksContainer = document.querySelector(".memory-game-blocks");
-
-// Create Array From Game Blocks
-let blocks = Array.from(blocksContainer.children);
-
-// Create Range Of Keys
-// let orderRange = [...Array(blocks.length).keys()];
-
+let blocks = [...blocksContainer.children];
 let orderRange = Array.from(Array(blocks.length).keys());
-
-// console.log(orderRange);
-shuffle(orderRange);
-// console.log(orderRange);
-
-// Add Order Css Property To Game Blocks
+orderRange.sort(() => 0.5 - Math.random());
+console.log(orderRange);
 blocks.forEach((block, index) => {
-
-    // Add CSS Order Property
     block.style.order = orderRange[index];
-
-    // Add Click Event
     block.addEventListener('click', function() {
-
-        // Trigger The Flip Block Function
         flipBlock(block);
-
     });
-
 });
 
-// Flip Block Function
 function flipBlock(selectedBlock) {
-
-    // Add Class is-flipped
     selectedBlock.classList.add('is-flipped');
-
-    // Collect All Flipped Cards
     let allFlippedBlocks = blocks.filter(flippedBlock => flippedBlock.classList.contains('is-flipped'));
-
-    // If Theres Two Selected Blocks
     console.log(allFlippedBlocks.length);
     if (allFlippedBlocks.length === 2) {
-
-
-        // console.log('Two Flipped Blocks Selected');
-
-        // Stop Clicking Function
         stopClicking();
-
-        // Check Matched Block Function
         checkMatchedBlocks(allFlippedBlocks[0], allFlippedBlocks[1]);
-        console.log("welcome here");
     }
 }
 
-
-
-// Stop Clicking Function
 function stopClicking() {
-
-    // Add Class No Clicking on Main Container
     blocksContainer.classList.add('no-clicking');
-
-    // Wait Duration
     setTimeout(() => {
-
-        // Remove Class No Clicking After The Duration
         blocksContainer.classList.remove('no-clicking');
-
     }, duration);
-
 }
-
 let cardsWon = 0;
 
-// Check Matched Block
 function checkMatchedBlocks(firstBlock, secondBlock) {
-
-
-    if (firstBlock.dataset.technology === secondBlock.dataset.technology) {
+    if (firstBlock.dataset.set === secondBlock.dataset.set) {
         for (let i = 0; i <= 6; i++) {
             cardsWon++
         }
-        console.log(`cardsWon is : ${cardsWon}`);
         if (cardsWon === 42) {
             setTimeout(() => {
                 location.reload();
             }, 2500);
-
         }
         firstBlock.classList.remove('is-flipped');
         secondBlock.classList.remove('is-flipped');
-
         firstBlock.classList.add('has-match');
         secondBlock.classList.add('has-match');
-
         document.getElementById('success').play();
-
     } else {
-
-
         setTimeout(() => {
-
             firstBlock.classList.remove('is-flipped');
             secondBlock.classList.remove('is-flipped');
-
         }, duration);
-
-
     }
-
 }
-
-// Shuffle Function
-function shuffle(array) {
-
-    // Settings Vars
-    let current = array.length,
-        temp,
-        random;
-
-    while (current > 0) {
-
-        // Get Random Number
-        random = Math.floor(Math.random() * current);
-
-        // Decrease Length By One
-        current--;
-
-        // [1] Save Current Element in Stash
-        temp = array[current];
-
-        console.log(temp)
-            // [2] Current Element = Random Element
-        array[current] = array[random];
-
-        // [3] Random Element = Get Element From Stash
-        array[random] = temp;
-        // console.log(Current);
-
-    }
-    return array;
-
-}
-// Current Array [9, 2, 10, 4, 5, 6, 7, 3, 1, 8]
-/*
-  [1] Save Current Element in Stash
-  [2] Current Element = Random Element
-  [3] Random Element = Get Element From Stash
-*/
